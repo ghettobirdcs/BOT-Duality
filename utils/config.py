8ac92b310422c1598f8d0858c7ec3c34cb9118c1
McKade Hinshaw <hinshaw.mckade@gmail.com>
2025-05-18 02:52:59 +0000

    removed local env vars

--- utils/config.py
+++ utils/config.py
@@ -6,14 +6,3 @@ import os
 load_dotenv()
 
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
-REACTION_MESSAGE_ID = int(os.getenv("REACTION_MESSAGE_ID", 0))
-AI_ROLE_NAME = os.getenv("AI_ROLE_NAME", "AI Fanclub")
-NSFW_CHANNEL = int(os.getenv("NSFW_CHANNEL", 0))
-MODEL_PATH = os.getenv("MODEL_PATH") 
-HF_TOKEN = os.getenv("HF_TOKEN")
-
-def is_in_allowed_channel():
-    def predicate(ctx):
-        # Check if the command is in the allowed channel or a dm
-        return isinstance(ctx.channel, discord.DMChannel) or ctx.channel.id == NSFW_CHANNEL
-    return commands.check(predicate)
