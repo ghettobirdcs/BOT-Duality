241df3314fbb1fa9596b0cc11e302a429cea550c
McKade Hinshaw <hinshaw.mckade@gmail.com>
2025-05-18 19:17:33 +0000

    fixed keep_alive.py

--- utils/keep_alive.py
+++ utils/keep_alive.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from threading import Thread
+import time
 
 app = Flask('')
 
@@ -10,6 +11,12 @@ def home():
 def run():
     app.run(host='0.0.0.0', port=8080)
 
+def keep_terminal_active():
+    while True:
+        print("Keeping terminal active...")
+        time.sleep(300)  # Print every 5 minutes
+
 def keep_alive():
+    Thread(target=keep_terminal_active, daemon=True).start()
     t = Thread(target=run)
     t.start()
