15f2fabaa3c5df8cbd025a977bb1dad4a0364214
McKade Hinshaw <hinshaw.mckade@gmail.com>
2025-05-18 06:32:26 +0000

    Added keep_alive.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@
 # TODO: See if there's a way to play music from spotify with a command
 
 from utils.config import DISCORD_TOKEN
+from utils.keep_alive import keep_alive
 from discord.ext import commands
 import asyncio
 import discord
@@ -60,4 +61,5 @@ async def main():
         await bot.start(DISCORD_TOKEN)  # pyright: ignore
 
 if __name__ == "__main__":
+    keep_alive()
     asyncio.run(main())
