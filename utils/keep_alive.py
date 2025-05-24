18c077064f1a26d259599c092ea32fb89a60bda4
McKade Hinshaw <hinshaw.mckade@gmail.com>
2025-05-24 10:44:27 -0600

    Updated port for Render.com

--- utils/keep_alive.py
+++ utils/keep_alive.py
@@ -3,13 +3,14 @@ from threading import Thread
 import time
 
 app = Flask('')
+port = process.env.port || 4000 # For running as a web app on render.com
 
 @app.route('/')
 def home():
     return "Bot is alive!"
 
 def run():
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=port)
 
 def keep_terminal_active():
     while True:
