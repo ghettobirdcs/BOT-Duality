f46315bf2566542882a250701770594d9a8022d1
McKade Hinshaw <hinshaw.mckade@gmail.com>
2025-06-05 17:10:00 -0600

    no new prospects

--- cogs/welcome.py
+++ cogs/welcome.py
@@ -7,16 +7,9 @@ class Welcome(commands.Cog):
 
     @commands.Cog.listener()
     async def on_member_join(self, member):
-        guild = member.guild
-        role = discord.utils.get(guild.roles, name="Prospects")
         # TODO: See if we can get 'general' channel without hardcoded id
         general = self.bot.get_channel(1197071765927637084)
-
-        if role:
-            await member.add_roles(role)
-            await general.send(f"Welcome to Duality, {member.mention}! You have been assigned the **{role.name}** role.")
-        else:
-            await general.send("Welcome to Duality! However, the **Prospects** role could not be assigned because it doesn't exist.")
+        await general.send(f"Welcome to Duality, {member.mention}!")
 
 async def setup(bot):
     await bot.add_cog(Welcome(bot))
